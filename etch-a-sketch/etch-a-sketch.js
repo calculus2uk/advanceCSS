443dd5aa141afa29f9a9118b50704939606dfc6b
Isaac Yao Tuagba <calculus2uk@gmail.com>
2020-04-27 16:18:56 +0300

    select the main elements

--- etch-a-sketch/etch-a-sketch.js
+++ etch-a-sketch/etch-a-sketch.js
@@ -1,1 +1,7 @@
 'use script';
+// Selects the elements on the page - canvas, shake and button
+
+const canvas = document.querySelector('#etch-a-sketch'),
+	ctx = canvas.getContext('2d');
+
+console.log(ctx);
