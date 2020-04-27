4b70cb1c276ce0843fa445d5fa04bab5172f2585
Isaac Yao Tuagba <calculus2uk@gmail.com>
2020-04-27 18:23:20 +0300

    Add functionality for path to change color

--- etch-a-sketch/etch-a-sketch.js
+++ etch-a-sketch/etch-a-sketch.js
@@ -4,19 +4,20 @@
 const canvas = document.querySelector('#etch-a-sketch'),
 	ctx = canvas.getContext('2d'),
 	shakeButton = document.querySelector('.shake'),
-	MOVE_AMOUNT = 10;
-
+	MOVE_AMOUNT = 20;
 //2. Setup the canvas for drawing
 const { width, height } = canvas; // make variables from destructuring the width and height from the canvas object
 
 //Create a random point from
-let x = Math.floor(Math.random() * width);
-let y = Math.floor(Math.random() * height);
+let x = Math.floor(Math.random() * width),
+	y = Math.floor(Math.random() * height),
+	HUE = 0;
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = MOVE_AMOUNT;
 
+ctx.strokeStyle = `hsl(100, 100%, 50%)`;
 ctx.beginPath(); // Start the drawing marker
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
@@ -24,6 +25,10 @@ ctx.stroke();
 
 //3. Write a draw function
 function draw({ key }) {
+	//Increment the hu value
+	HUE += 10;
+	// Change  the color of the path
+	ctx.strokeStyle = `hsl(${HUE}, 100%, 50%)`;
 	// Begin to /Start the path
 	ctx.beginPath();
 	ctx.moveTo(x, y);
