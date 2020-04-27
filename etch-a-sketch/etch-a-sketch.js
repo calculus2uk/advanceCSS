9a46688bff0ef9c5088f96a8a8864251ebec0532
Isaac Yao Tuagba <calculus2uk@gmail.com>
2020-04-27 16:31:32 +0300

    set up the canvas for drawing

--- etch-a-sketch/etch-a-sketch.js
+++ etch-a-sketch/etch-a-sketch.js
@@ -1,7 +1,26 @@
 'use script';
-// Selects the elements on the page - canvas, shake and button
 
+//1. Selects the elements on the page - canvas, shake and button
 const canvas = document.querySelector('#etch-a-sketch'),
-	ctx = canvas.getContext('2d');
+	ctx = canvas.getContext('2d'),
+	shakeButton = document.querySelector('.shake');
 
-console.log(ctx);
+//2. Setup the canvas for drawing
+const { width, height } = canvas; // make variables from destructuring the width and height from the canvas object
+
+//Create a random point from
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+
+ctx.beginPath(); // Start the drawing marker
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke();
+
+//3. Write a draw function
+
+//4.
